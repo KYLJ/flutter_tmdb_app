89d9c66a08736e79acc8d78750e26d184d91d3ad
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-04-20 00:51:25 +0900

    fix: change data source method return type

--- lib/data/data_source/popular_movie_data_source.dart
+++ lib/data/data_source/popular_movie_data_source.dart
@@ -1,5 +1,5 @@
 import '../dto/popular_movie_dto.dart';
 
 abstract interface class PopularDataSource {
-  Future<ResultDto> getPopularMovieApi();
+  Future<PopularMovieDto> getPopularMovieApi();
 }
