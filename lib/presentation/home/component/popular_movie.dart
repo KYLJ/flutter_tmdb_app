70042405c3cad8359dbe1dc0c918877103f95ab9
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-04-23 02:05:00 +0900

    fix: change incorrect import link

--- lib/presentation/home/component/popular_movie.dart
+++ lib/presentation/home/component/popular_movie.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_tmdb_app/presentation/popular_movie/popular_movie_view_model.dart';
 import 'package:provider/provider.dart';
 
-import '../../../data/dto/now_playing_movie_dto.dart';
+import '../../../data/dto/popular_movie_dto.dart';
+import '../../popular_movie/genres_list.dart';
 
 class PopularMovie extends StatefulWidget {
   const PopularMovie({super.key});
@@ -22,132 +23,123 @@ class _PopularMovieState extends State<PopularMovie> {
   @override
   Widget build(BuildContext context) {
     PopularMovieViewModel viewModel = context.watch();
-    return Column(
-      children: [
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Popular',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
-                ),
-                SizedBox(
-                  height: 22.0,
-                  width: 64.0,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                        side: const BorderSide(width: 0.4, color: Colors.grey),
-                        padding: const EdgeInsets.all(2)),
-                    child: const Text(
-                      'See more',
-                      style: TextStyle(fontSize: 10.0, color: Colors.grey),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        SliverList.builder(
-          itemCount: viewModel.popularMovieList.length,
-          itemBuilder: (BuildContext context, int index) {
-            ResultDto result = viewModel.popularMovieList[index];
-            return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24.0),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+    double screenWidth = MediaQuery.of(context).size.width;
+    return SliverList.builder(
+      itemCount: viewModel.popularMovieList.length,
+      itemBuilder: (BuildContext context, int index) {
+        ResultDto result = viewModel.popularMovieList[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: SizedBox(
+                  width: 92.0,
+                  child: AspectRatio(
+                    aspectRatio: 2 / 3,
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(5.0),
                       child: Image.network(
                         '$imageBaseUrl${result.posterPath}',
-                        // height: 128.0,
-                        width: 60.0,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                    child: SizedBox(
-                      height: 128.0,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '${result.title}',
-                            style: const TextStyle(
-                                fontSize: 17.0, fontWeight: FontWeight.w600),
-                          ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                color: Colors.yellow.shade700,
-                                size: 20.0,
-                              ),
-                              const SizedBox(width: 8.0),
-                              Text(
-                                result.voteAverage != null
-                                    ? '${result.voteAverage!.toStringAsFixed(1)} / 10'
-                                    : 'No rating',
-                                style: TextStyle(
-                                    fontSize: 16.0,
-                                    color: Colors.grey.shade600),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(100),
-                                  color:
-                                      Colors.indigo.shade100.withOpacity(0.7),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 6.0, horizontal: 16.0),
-                                  child: Text(
-                                    '${result.genreIds}',
-                                    style: TextStyle(
-                                        fontSize: 12.0,
-                                        color: Colors.indigo.shade400),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              const Icon(
-                                Icons.access_time,
-                                size: 20.0,
-                              ),
-                              const SizedBox(width: 8.0),
-                              Text(
-                                '${result.releaseDate}',
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 16.0),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
-            );
-          },
-        ),
-      ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: SizedBox(
+                  // height: 128.0,
+                  width: screenWidth - 180,
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 2.0),
+                        child: Text(
+                          '${result.title}',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: true,
+                          style: const TextStyle(
+                              fontSize: 16.0, fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 6.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow.shade700,
+                              size: 20.0,
+                            ),
+                            const SizedBox(width: 8.0),
+                            Text(
+                              result.voteAverage != null
+                                  ? '${result.voteAverage!.toStringAsFixed(1)} / 10'
+                                  : 'No rating',
+                              style: TextStyle(
+                                  fontSize: 16.0, color: Colors.grey.shade600),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Wrap(
+                        spacing: 8.0, // 수평 방향 간격
+                        runSpacing: 8.0, // 수직 방향 간격
+                        children: List<Widget>.generate(
+                          result.genreIds?.length ?? 0,
+                          (int index) {
+                            final genresId = genresList.firstWhere((element) =>
+                                element['id'] == result.genreIds![index]);
+                            return Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: Colors.indigo.shade100.withOpacity(0.7),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 3.0, horizontal: 12.0),
+                                child: Text(
+                                  '${genresId['name']}',
+                                  style: TextStyle(
+                                      color: Colors.indigo.shade400,
+                                      fontSize: 12.0,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 6.0),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.access_time,
+                              size: 20.0,
+                            ),
+                            const SizedBox(width: 8.0),
+                            Text(
+                              '${result.releaseDate}',
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400, fontSize: 16.0),
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
