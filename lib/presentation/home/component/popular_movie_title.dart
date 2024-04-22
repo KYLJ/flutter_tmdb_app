56212de91bb1ed335c3471078d97e65a08290e62
yujiyeong <jiyeong.yu.kr@gmail.com>
2024-04-23 02:03:08 +0900

    feat : create popular movie title component

---
+++ lib/presentation/home/component/popular_movie_title.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class PopularMovieTitle extends StatelessWidget {
+  const PopularMovieTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text(
+              'Popular',
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 22.0,
+              width: 64.0,
+              child: OutlinedButton(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                    side: const BorderSide(width: 0.4, color: Colors.grey),
+                    padding: const EdgeInsets.all(2)),
+                child: const Text(
+                  'See more',
+                  style: TextStyle(fontSize: 10.0, color: Colors.grey),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
