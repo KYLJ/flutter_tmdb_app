8c77f9a2bf080a5357064dbe89ef776f526fc415
leeseowoo <leeseowoo.kr@gmail.com>
2024-04-23 16:02:47 +0900

    feat: apply merriweather font

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tmdb_app/presentation/home/component/now_playing_movie.dart';
 import 'package:flutter_tmdb_app/presentation/home/component/popular_movie.dart';
 import 'package:flutter_tmdb_app/presentation/home/component/popular_movie_title.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'component/now_playing_movie_title.dart';
 
@@ -11,14 +11,14 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
             floating: true,
             backgroundColor: Colors.white54,
-            leading: Icon(Icons.menu),
-            actions: [
+            leading: const Icon(Icons.menu),
+            actions: const [
               Padding(
                 padding: EdgeInsets.all(12.0),
                 child: Icon(Icons.search),
@@ -27,22 +27,24 @@ class HomeScreen extends StatelessWidget {
             title: Center(
               child: Text(
                 'KYLJ Cinema',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
+                style: GoogleFonts.merriweather(
+                  textStyle: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
           ),
-          SliverToBoxAdapter(
+          const SliverToBoxAdapter(
             child: SizedBox(
               height: 25,
             ),
           ),
-          NowPlayingMovieTitle(),
-          NowPlayingMovie(),
-          PopularMovieTitle(),
-          PopularMovie(),
+          const NowPlayingMovieTitle(),
+          const NowPlayingMovie(),
+          const PopularMovieTitle(),
+          const PopularMovie(),
         ],
       ),
     );
